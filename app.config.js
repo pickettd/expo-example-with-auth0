eaaec13cc7de9c383d404bce6d4db2b3dc916907
David Pickett <pickettd@gmail.com>
2021-06-19 13:33:08 -1000

    Add additional env var config for embedded login

--- app.config.js
+++ app.config.js
@@ -4,6 +4,8 @@ export default ({ config }) => {
   const extra = { ...config.extra };
   extra.auth0ClientId = process.env.AUTH0_CLIENT_ID;
   extra.auth0Domain = process.env.AUTH0_DOMAIN;
+  extra.auth0ApiAudience = process.env.AUTH0_API_AUDIENCE;
+  extra.auth0Realm = process.env.AUTH0_REALM;
   config.extra = extra;
   return {
     ...config,
