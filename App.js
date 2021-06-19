d9e90ac8157d6399cbbc7aa0f63e836594ce8f73
David Pickett <pickettd@gmail.com>
2021-06-18 16:08:10 -1000

    Replace the app code with a kinda-broken passwordless version

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
+// Note this implementation only works in the browser
+
+import React, { useEffect, useState } from "react";
+import auth0 from "auth0-js";
 import * as AuthSession from "expo-auth-session";
 import * as WebBrowser from "expo-web-browser";
-import jwtDecode from "jwt-decode";
-import * as React from "react";
-import { Alert, Button, Platform, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+} from "react-native";
 import Constants from "expo-constants";
 
-// You need to swap out the Auth0 client id and domain with the one from your Auth0 client.
-// In your Auth0 client, you need to also add a url to your authorized redirect urls.
-//
-// For this application, I added https://auth.expo.io/@arielweinberger/with-auth0 because I am
-// signed in as the 'arielweinberger' account on Expo and the name/slug for this app is 'with-auth0'.
-//
-// You can open this app in the Expo client and check your logs to find out your redirect URL.
+WebBrowser.maybeCompleteAuthSession();
 
 const auth0ClientId = Constants.manifest.extra.auth0ClientId || "";
 const auth0Domain = Constants.manifest.extra.auth0Domain || "";
@@ -20,52 +24,93 @@ const authorizationEndpoint = "https://" + auth0Domain + "/authorize";
 const useProxy = Platform.select({ web: false, default: true });
 const redirectUri = AuthSession.makeRedirectUri({ useProxy });
 
-WebBrowser.maybeCompleteAuthSession();
+const webAuth = new auth0.WebAuth({
+  domain: auth0Domain,
+  clientID: auth0ClientId,
+  scope: "openid profile email",
+  responseType: "token id_token",
+  redirectUri,
+});
 
 export default function App() {
   const [name, setName] = React.useState(null);
+  const [email, setEmail] = React.useState(null);
+  const [text, onChangeText] = React.useState("Useless Text");
 
-  const [request, result, promptAsync] = AuthSession.useAuthRequest(
-    {
-      redirectUri,
-      clientId: auth0ClientId,
-      // id_token will return a JWT token
-      responseType: "id_token",
-      // retrieve the user's profile
-      scopes: ["openid", "profile"],
-      extraParams: {
-        // ideally, this will be a random value
-        nonce: "nonce",
-      },
-    },
-    { authorizationEndpoint }
-  );
-
-  // Retrieve the redirect URL, add this to the callback URL list
-  // of your Auth0 application.
-  console.log(`Redirect URL: ${redirectUri}`);
-
-  React.useEffect(() => {
-    if (result) {
-      if (result.error) {
-        Alert.alert(
-          "Authentication error",
-          result.params.error_description || "something went wrong"
-        );
-        return;
-      }
-      if (result.type === "success") {
-        // Retrieve the JWT token and decode it
-        const jwtToken = result.params.id_token;
-        const decoded = jwtDecode(jwtToken);
-
-        const { name } = decoded;
-        setName(name);
-      } else {
-        console.log("not error, not success");
-      }
+  webAuth.parseHash({ hash: window.location.hash }, function (err, authResult) {
+    if (err) {
+      return console.log(err);
     }
-  }, [result]);
+
+    webAuth.client.userInfo(authResult.accessToken, function (err, user) {
+      // Now you have the user's information
+      if (err) {
+        return console.log(err);
+      }
+      if (user && user.name) {
+        setName(user.name);
+      }
+    });
+  });
+
+  function onPasswordlessStart(enteredEmail) {
+    webAuth.passwordlessStart(
+      {
+        connection: "email",
+        send: "code",
+        email: enteredEmail,
+      },
+      function (err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(res);
+          setEmail(enteredEmail);
+          onChangeText("");
+        }
+        // handle errors or continue
+      }
+    );
+  }
+
+  function onPasswordlessVerify(enteredOTP) {
+    webAuth.passwordlessLogin(
+      {
+        connection: "email",
+        verificationCode: enteredOTP,
+        email,
+      },
+      function (err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(res);
+          webAuth.parseHash(
+            { hash: window.location.hash },
+            function (err, authResult) {
+              if (err) {
+                return console.log(err);
+              }
+
+              webAuth.client.userInfo(
+                authResult.accessToken,
+                function (err, user) {
+                  if (err) {
+                    return console.log(err);
+                  }
+                  // Now you have the user's information
+                  if (user && user.name) {
+                    setName(user.name);
+                  }
+                }
+              );
+            }
+          );
+        }
+        // handle errors or continue
+      }
+    );
+  }
 
   return (
     <View style={styles.container}>
@@ -74,12 +119,30 @@ export default function App() {
           <Text style={styles.title}>You are logged in, {name}!</Text>
           <Button title="Log out" onPress={() => setName(null)} />
         </>
+      ) : email ? (
+        <>
+          <Text>You entered this! {email}</Text>
+          <Text>Please type OTP:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+          />
+          <Button title="Verify" onPress={() => onPasswordlessVerify(text)} />
+        </>
       ) : (
-        <Button
-          disabled={!request}
-          title="Log in with Auth0"
-          onPress={() => promptAsync({ useProxy })}
-        />
+        <>
+          <Text style={styles.title}>Enter your email: </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+          />
+          <Button
+            title="Email me OTP"
+            onPress={() => onPasswordlessStart(text)}
+          />
+        </>
       )}
     </View>
   );
@@ -96,5 +159,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: "center",
     marginTop: 40,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
